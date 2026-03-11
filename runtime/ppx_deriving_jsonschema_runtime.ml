728f1f6f3cd6dcbefa43f6173f918d47836b71f8
Automated ocamlformat GitHub action, developed by robur.coop <autoformat@robur.coop>
2026-03-11 21:21:56 +0000

    formatted code

--- runtime/ppx_deriving_jsonschema_runtime.ml
+++ runtime/ppx_deriving_jsonschema_runtime.ml
@@ -3,15 +3,15 @@ let schema_version = "https://json-schema.org/draft/2020-12/schema"
 (* There is no Yojson.Basic.t in Melange and the Melange_json.t is not compatible with Yojson.Basic.t, so we use our own type to support universal API *)
 
 include struct
-    type t =
-      [ `Null
-      | `String of string
-      | `Float of float
-      | `Int of int
-      | `Bool of bool
-      | `List of t list
-      | `Assoc of (string * t) list
-      ]
+  type t =
+    [ `Null
+    | `String of string
+    | `Float of float
+    | `Int of int
+    | `Bool of bool
+    | `List of t list
+    | `Assoc of (string * t) list
+    ]
 end
 
 let json_schema ?id ?title ?description ?definitions types =
