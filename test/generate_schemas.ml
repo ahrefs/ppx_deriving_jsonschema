ed6ee1e791c1004e4ed296359838bec7699910a2
Louis Roché (Ahrefs) <louis.roche@ahrefs.com>
2026-04-24 10:18:42 +0000

    Inline native schema snapshot coercion

--- test/generate_schemas.ml
+++ test/generate_schemas.ml
@@ -1,3 +1,4 @@
-let yojson_of_json (json : Ppx_deriving_jsonschema_runtime.t) : Yojson.Basic.t = (json :> Yojson.Basic.t)
-
-let () = print_endline (Yojson.Basic.pretty_to_string (yojson_of_json Generate_schemas_cases.snapshot))
+let () =
+  print_endline
+    (Yojson.Basic.pretty_to_string
+       (Generate_schemas_cases.snapshot : Ppx_deriving_jsonschema_runtime.t :> Yojson.Basic.t))
