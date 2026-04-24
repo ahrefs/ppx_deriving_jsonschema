1a719a1007eece3194d427735caf1da1ce113207
Louis Roché (Ahrefs) <louis.roche@ahrefs.com>
2026-04-24 10:17:28 +0000

    Simplify native schema snapshot conversion

--- test/generate_schemas.ml
+++ test/generate_schemas.ml
@@ -1,10 +1,3 @@
-let rec yojson_of_json = function
-  | `Null -> `Null
-  | `String s -> `String s
-  | `Float f -> `Float f
-  | `Int i -> `Int i
-  | `Bool b -> `Bool b
-  | `List xs -> `List (List.map yojson_of_json xs)
-  | `Assoc fields -> `Assoc (List.map (fun (key, value) -> key, yojson_of_json value) fields)
+let yojson_of_json (json : Ppx_deriving_jsonschema_runtime.t) : Yojson.Basic.t = (json :> Yojson.Basic.t)
 
 let () = print_endline (Yojson.Basic.pretty_to_string (yojson_of_json Generate_schemas_cases.snapshot))
